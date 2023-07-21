f3b146502165e399955d306ee9ad578a01e010aa
Александр Лебедев <64793044+Sanch0pansa@users.noreply.github.com>
2023-07-21 11:02:46 +0300

    feat: добавление в роутинг основных страниц

--- frontend/src/client/src/app/router/config.jsx
+++ frontend/src/client/src/app/router/config.jsx
@@ -1,9 +1,46 @@
 import {createBrowserRouter} from "react-router-dom";
 import Layout from "../../pages/layout/index.js";
+import Main from "../../pages/main/index.js";
+import Profile from "../../pages/profile/index.js";
+import UserProfile from "../../pages/user-profile/index.js";
+import Login from "../../pages/login/index.js";
+import Registration from "../../pages/registration/index.js";
+import File from "../../pages/file/index.js";
+import Group from "../../pages/group/index.js";
 
 export const router = createBrowserRouter([
     {
         path: "/",
         element: <Layout />,
+        children: [
+            {
+                path: "/",
+                element: <Main/>,
+            },
+            {
+                path: "/profile",
+                element: <Profile/>,
+            },
+            {
+                path: "/user/:username",
+                element: <UserProfile />,
+            },
+            {
+                path: "/login",
+                element: <Login/>,
+            },
+            {
+                path: "/register",
+                element: <Registration/>,
+            },
+            {
+                path: "/file/:resource",
+                element: <File />,
+            },
+            {
+                path: "/group/:resource",
+                element: <Group/>,
+            },
+        ]
     },
 ]);
