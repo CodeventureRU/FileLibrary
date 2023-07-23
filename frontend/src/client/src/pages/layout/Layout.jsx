dd1475ef711b0c3b653901bfa8c7535ab4dc79e0
Александр Лебедев <64793044+Sanch0pansa@users.noreply.github.com>
2023-07-23 21:27:28 +0300

    fix(widgets): исправление работы с public API

--- frontend/src/client/src/pages/layout/Layout.jsx
+++ frontend/src/client/src/pages/layout/Layout.jsx
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 import {Outlet} from "react-router-dom";
-import Header from "../../widgets/header/Header.jsx";
+import {Header} from "../../widgets/header/index";
 
 const Layout = () => {
     const [isSidebarOpened, setIsSidebarOpened] = useState(false);
