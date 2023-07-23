9023f8d6fc078ce0633d0c79f53a42b7f2a72e01
Александр Лебедев <64793044+Sanch0pansa@users.noreply.github.com>
2023-07-23 23:03:44 +0300

    feat(pages): Обертывание содержимого layout в контейнер

--- frontend/src/client/src/pages/layout/Layout.jsx
+++ frontend/src/client/src/pages/layout/Layout.jsx
@@ -1,14 +1,14 @@
 import React, {useState} from 'react';
 import {Outlet} from "react-router-dom";
 import {Header} from "../../widgets/header/index";
+import {Container} from "@mui/material";
 
 const Layout = () => {
     const [isSidebarOpened, setIsSidebarOpened] = useState(false);
     return (
         <>
             <Header toggleMenu={() => setIsSidebarOpened(!isSidebarOpened)} />
-            <Outlet />
-
+            <Container maxWidth="md"><Outlet /></Container>
         </>
     );
 };
