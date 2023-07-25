7f7f9e77f8244611b1c3856a32303ce24246c275
Александр Лебедев <64793044+Sanch0pansa@users.noreply.github.com>
2023-07-25 13:37:34 +0300

    feat(shared,api): работа с csrf-кукой и изменение адреса сервера

--- frontend/src/client/src/shared/api/api.js
+++ frontend/src/client/src/shared/api/api.js
@@ -1,12 +1,35 @@
 import axios from "axios";
 import {useState} from "react";
 
-export const API_URL = 'http://127.0.0.1:8000/api/v1';
+export const API_URL = 'http://localhost:8000/api/v1';
 
 const $api = axios.create({
     withCredentials: true,
     baseURL: API_URL,
 });
+
+function getCookie(c_name) {
+    if (document.cookie.length > 0) {
+        let c_start = document.cookie.indexOf(c_name + "=");
+        if (c_start !== -1) {
+            c_start = c_start + c_name.length + 1;
+            let c_end = document.cookie.indexOf(";", c_start);
+            if (c_end === -1) {
+                c_end = document.cookie.length;
+            }
+            return unescape(document.cookie.substring(c_start, c_end));
+        }
+    }
+    return "";
+}
+
+const setCsrf = () => {
+    let csrf = getCookie('csrftoken');
+    if (csrf) {
+        $api.defaults.headers['X-CSRFTOKEN'] = csrf;
+    }
+}
+setCsrf();
 
 export const useApi = (url, method) => {
     const [loading, setLoading] = useState(false);
@@ -40,6 +63,7 @@ export const useApi = (url, method) => {
                     res = res.data;
                     break;
             }
+            setCsrf(res);
         } catch (e) {
             if (e?.response?.data) {
                 setErrors(e.response.data);
