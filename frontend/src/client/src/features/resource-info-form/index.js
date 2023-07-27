c9c59bfa4a14bbf3c25c30c4e1153c93e4c0aa6c
Александр Лебедев <64793044+Sanch0pansa@users.noreply.github.com>
2023-07-27 16:39:32 +0300

    feat(features,resource-info-form): создание формы основной информации ресурса

---
+++ frontend/src/client/src/features/resource-info-form/index.js
@@ -0,0 +1,20 @@
+import ResourceInfoForm from "./ResourceInfoForm.jsx";
+import {useState} from "react";
+
+const useResourceInfoForm = (values={}) => {
+    const [name, setName] = useState(values.name ? values.name : "");
+    const [description, setDescription] = useState(values.description ? values.description : "");
+    const [usingImage, setUsingImage] = useState(Boolean(values.image));
+    const [image, setImage] = useState(values.image ? values.image : null);
+    const [imageError, setImageError] = useState(null);
+
+    return {
+        name, setName,
+        description, setDescription,
+        usingImage, setUsingImage,
+        image, setImage,
+        imageError, setImageError,
+    }
+}
+
+export {ResourceInfoForm, useResourceInfoForm};
