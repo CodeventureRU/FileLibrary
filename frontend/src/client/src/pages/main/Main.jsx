18d91845d69570361c42952739e02f86eac73d6f
Александр Лебедев <64793044+Sanch0pansa@users.noreply.github.com>
2023-07-29 23:34:17 +0300

    feat(pages,main): добавление созданных компонентов на главную страницу

--- frontend/src/client/src/pages/main/Main.jsx
+++ frontend/src/client/src/pages/main/Main.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import {MainResourcesList} from "../../widgets/main-resources-list/index";
+import {MainPageScreen} from "../../widgets/main-page-screen/index.js";
 
 const Main = () => {
     return (
-        <div>
-            
-        </div>
+        <>
+            <MainPageScreen />
+            <MainResourcesList />
+        </>
     );
 };
 
