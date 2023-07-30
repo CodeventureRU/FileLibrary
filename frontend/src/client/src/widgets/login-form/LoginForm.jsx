2ef8f0ce60e9a8802ed5f890e9a3fa6e6c284d39
Александр Лебедев <64793044+Sanch0pansa@users.noreply.github.com>
2023-07-30 18:06:11 +0300

    feat(widgets,shared): добавление в форму входа ссылки на сброс пароля и изменение стиля

--- frontend/src/client/src/widgets/login-form/LoginForm.jsx
+++ frontend/src/client/src/widgets/login-form/LoginForm.jsx
@@ -7,7 +7,7 @@ import {
     Paper,
     Typography
 } from "@mui/material";
-import {useNavigate} from "react-router-dom";
+import {NavLink, useNavigate} from "react-router-dom";
 import {ErrorsBag} from "../../shared/ui/errors-bag/index";
 import {LoadingButton} from "../../shared/ui/loading-button/index.js";
 import {GridFormControl} from "../../shared/ui/grid-form-control/index.js";
@@ -32,12 +32,13 @@ const LoginForm = () => {
 
 
     return (
+        <>
         <Grid container sx={{justifyContent: 'center', mt: 5}}>
             <Grid item xs={12} md={6}>
-                <Paper sx={{p: 5}}>
+                <Paper sx={{p: 5}} variant="outlined" elevation={0}>
                     <Typography variant="h5" sx={{textAlign: 'center'}}>Вход</Typography>
                     <Box
-                        sx={{my: 5}}
+                        sx={{mt: 5}}
                     >
                         <Grid container spacing={2}>
 
@@ -59,6 +60,16 @@ const LoginForm = () => {
                                 }}
                                 errors={errors?.password}
                             />
+                            <Grid item xs={12} sx={{}}>
+                                <NavLink style={{textDecoration: 'none', color: 'inherit'}} to={"/send-reset-password"}>
+                                    <Typography variant="body2" color="primary">Забыли пароль?</Typography>
+                                </NavLink>
+                            </Grid>
+                            {/*<Grid item xs={12} sx={{display: 'flex', justifyContent: 'center'}}>*/}
+                            {/*    <NavLink style={{textDecoration: 'none', color: 'inherit'}} to={"/send-reset-password"}>*/}
+                            {/*        <Typography variant="caption" color="primary">Забыли пароль?</Typography>*/}
+                            {/*    </NavLink>*/}
+                            {/*</Grid>*/}
 
                             <Grid item xs={12}>
                                 <ErrorsBag errors={detailsErrors} setErrors={setDetailsErrors}></ErrorsBag>
@@ -67,16 +78,14 @@ const LoginForm = () => {
                         <Box
                             sx={{display: 'flex', alignItems: 'center', justifyContent: 'space-between', mt: 5}}
                         >
-                            <Button href="/register">Зарегистрироваться</Button>
-                            <LoadingButton loading={loading} onClick={loginHandle}>Войти</LoadingButton>
+                            <Button sx={{ml: -1, textTransform: 'none'}} href="/register">Зарегистрироваться</Button>
+                            <LoadingButton loading={loading} onClick={loginHandle} buttonProps={{sx: {textTransform: 'none'}}}>Войти</LoadingButton>
                         </Box>
                     </Box>
-
                 </Paper>
             </Grid>
-
         </Grid>
-
+        </>
     );
 };
 
