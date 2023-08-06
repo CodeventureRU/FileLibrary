9f5f5649a92d0440a843132edbe01765f6f37ff9
Александр Лебедев <64793044+Sanch0pansa@users.noreply.github.com>
2023-08-06 19:59:06 +0300

    feat(widgets): создание формы редактирования группы

--- frontend/src/client/src/widgets/resource-forms/index.js
+++ frontend/src/client/src/widgets/resource-forms/index.js
@@ -1,5 +1,6 @@
 import CreatingFileForm from "./CreatingFileForm.jsx";
 import CreatingGroupForm from "./CreatingGroupForm.jsx";
 import EditingFileForm from "./EditingFileForm.jsx";
+import EditingGroupForm from "./EditingGroupForm";
 
-export {EditingFileForm, CreatingGroupForm, CreatingFileForm}
+export {EditingFileForm, CreatingGroupForm, CreatingFileForm, EditingGroupForm}
