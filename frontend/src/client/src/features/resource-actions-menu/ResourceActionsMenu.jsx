53836ff666a8f15fc1ebf63d43130cddc35bfff9
Александр Лебедев <64793044+Sanch0pansa@users.noreply.github.com>
2023-08-07 16:16:38 +0300

    feat(feature): реализация фичи удаления ресурса из контекстного меню

--- frontend/src/client/src/features/resource-actions-menu/ResourceActionsMenu.jsx
+++ frontend/src/client/src/features/resource-actions-menu/ResourceActionsMenu.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
 import {NavLink} from "react-router-dom";
-import {ListItemIcon, ListItemText, Menu, MenuItem, Typography} from "@mui/material";
+import {
+    Button,
+    Dialog, DialogActions,
+    DialogContent, DialogContentText,
+    DialogTitle,
+    ListItemIcon,
+    ListItemText,
+    Menu,
+    MenuItem,
+    Typography
+} from "@mui/material";
 import {Delete, Edit, Folder} from "@mui/icons-material";
+import {removeResourceSelector, useRemoveResource, useResourcesStore} from "../../entities/resource/index.js";
 
 const ResourceActionsMenu = ({
     resource,
@@ -12,6 +23,7 @@ const ResourceActionsMenu = ({
     addGroupsAction=true,
 }) => {
 
+    const {removeResourceRequest} = useRemoveResource(resource ? resource.slug : "");
     const menuOpen = Boolean(element);
 
     const resourceEditLink = resource ? (
@@ -20,53 +32,94 @@ const ResourceActionsMenu = ({
         (resource.slug ? resource.slug : resource.id)
     ) : "";
 
+    const removeResource = useResourcesStore(removeResourceSelector);
+    const handleRemove = async () => {
+        await removeResourceRequest();
+        removeResource(resource.slug);
+        handleClose();
+    }
+
+    // Упраление модальным окном подтверждения удаления
+    const [open, setOpen] = React.useState(false);
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+        close();
+    };
+
+
     return (
-        <Menu
-            id="resource-actions-menu"
-            anchorEl={element}
-            open={menuOpen}
-            onClose={close}
-            MenuListProps={{
-                'aria-labelledby': 'resource-actions-menu',
-            }}
-        >
-            {
-                editAction ? (
-                    <NavLink to={resourceEditLink} style={{color: "inherit", textDecoration: 'none'}}>
-                        <MenuItem onClick={close}>
-                            <ListItemIcon>
-                                <Edit fontSize="small" />
-                            </ListItemIcon>
-                            <ListItemText>Редактировать</ListItemText>
-                        </MenuItem>
-                    </NavLink>
-                ) : ""
-            }
-            {
-                (resource && addGroupsAction) ? (
-                  resource.type === 'file' ? (
-                      <MenuItem onClick={close}>
-                          <ListItemIcon>
-                              <Folder fontSize="small" />
-                          </ListItemIcon>
-                          <ListItemText>Группы</ListItemText>
-                      </MenuItem>
-                  ) : ""
-               ) : ""
-            }
-            {
-                deleteAction ? (
-                    <NavLink to={resourceEditLink} style={{color: "inherit", textDecoration: 'none'}}>
-                        <MenuItem onClick={close}>
+        <>
+            <Menu
+                id="resource-actions-menu"
+                anchorEl={element}
+                open={menuOpen}
+                onClose={close}
+                MenuListProps={{
+                    'aria-labelledby': 'resource-actions-menu',
+                }}
+            >
+                {
+                    editAction ? (
+                        <NavLink to={resourceEditLink} style={{color: "inherit", textDecoration: 'none'}}>
+                            <MenuItem onClick={close}>
+                                <ListItemIcon>
+                                    <Edit fontSize="small" />
+                                </ListItemIcon>
+                                <ListItemText>Редактировать</ListItemText>
+                            </MenuItem>
+                        </NavLink>
+                    ) : ""
+                }
+                {
+                    (resource && addGroupsAction) ? (
+                      resource.type === 'file' ? (
+                          <MenuItem onClick={close}>
+                              <ListItemIcon>
+                                  <Folder fontSize="small" />
+                              </ListItemIcon>
+                              <ListItemText>Группы</ListItemText>
+                          </MenuItem>
+                      ) : ""
+                   ) : ""
+                }
+                {
+                    deleteAction ? (
+                        <MenuItem onClick={handleClickOpen}>
                             <ListItemIcon>
                                 <Delete color="error" fontSize="small" />
                             </ListItemIcon>
                             <ListItemText><Typography color="error">Удалить</Typography></ListItemText>
                         </MenuItem>
-                    </NavLink>
-                ) : ""
-            }
-        </Menu>
+                    ) : ""
+                }
+            </Menu>
+            <Dialog
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="confirmation-dialog-title"
+                aria-describedby="confirmation-dialog-description"
+            >
+                <DialogTitle id="confirmation-dialog-title">
+                    {`Вы действительно хотите удалить ${resource ? (resource.type === 'file' ? 'Файл' : "Группу") : ""}?`}
+                </DialogTitle>
+                <DialogContent>
+                    <DialogContentText id="confirmation-dialog-description">
+                        {`Восстановить ${resource ? (resource.type === 'file' ? 'удаленный файл' : "удаленную группу") : ""} будет невозможно`}
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button color="error" variant="outlined" onClick={handleClose}>Отмена</Button>
+                    <Button color="error" variant="contained" onClick={handleRemove} autoFocus>
+                        Удалить
+                    </Button>
+                </DialogActions>
+            </Dialog>
+        </>
     );
 };
 
