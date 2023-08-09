e4541cf98f01efb816c8575959c551f9e0c0314e
Александр Лебедев <64793044+Sanch0pansa@users.noreply.github.com>
2023-08-09 14:14:35 +0300

    fix(widgets): исправления отображения добавления в группы у неавторизованных пользователей

--- frontend/src/client/src/widgets/resources-lists/ui/GroupResourcesList.jsx
+++ frontend/src/client/src/widgets/resources-lists/ui/GroupResourcesList.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import {useFetchGroupResources} from "../../../entities/resource/index.js";
 import ResourcesListTemplate from "./ResourcesListTemplate.jsx";
+import {isAuthSelector, useViewerStore} from "../../../entities/viewer/index.js";
 
 const GroupResourcesList = ({resource}) => {
+    const isAuth = useViewerStore(isAuthSelector);
     const resourcesHook = useFetchGroupResources(resource);
 
     return (
         <ResourcesListTemplate
             resourcesHook={resourcesHook}
             showEditAction={false}
-            showAddToGroupAction={true}
+            showAddToGroupAction={isAuth}
             showFavoriteAction={true}
             showDownloads={true}
         />
--- frontend/src/client/src/widgets/resources-lists/ui/UserResourcesList.jsx
+++ frontend/src/client/src/widgets/resources-lists/ui/UserResourcesList.jsx
@@ -2,16 +2,18 @@ import React from 'react';
 import {useFetchUserResources} from "../../../entities/resource/index.js";
 import ResourcesListTemplate from "./ResourcesListTemplate.jsx";
 import {useParams} from "react-router-dom";
+import {isAuthSelector, useViewerStore} from "../../../entities/viewer/index.js";
 
 const UserResourcesList = () => {
     const {username} = useParams();
+    const isAuth = useViewerStore(isAuthSelector);
     const resourcesHook = useFetchUserResources(username);
 
     return (
         <ResourcesListTemplate
             resourcesHook={resourcesHook}
             showEditAction={false}
-            showAddToGroupAction={true}
+            showAddToGroupAction={isAuth}
             showFavoriteAction={true}
             showDownloads={true}
         />
