aca240d17633a3a27f9fe0109e70486f95d06f65
Ante <coderasylum@gmail.com>
2023-08-09 17:01:19 +0300

    feat: добавление сервиса для удаления неактивных пользователей

---
+++ server/src/project/API/management/commands/user_cleaner.py
@@ -0,0 +1,32 @@
+import time
+import schedule
+from datetime import timedelta
+from django.core.management.base import BaseCommand
+from django.utils import timezone
+
+from API.models import User
+
+
+class Command(BaseCommand):
+
+    def job(self):
+        queryset = User.objects.filter(is_active=False).values('id', 'username', 'last_login')
+        for user_info in queryset:
+            time_now = timezone.now()
+            difference = time_now - user_info['last_login']
+            if difference > timedelta(days=1):
+
+                user = User.objects.get(pk=user_info['id'])
+                user.delete()
+                print(f'[{time_now}] Deleted: id: {user_info["id"]}, '
+                      f'username: {user_info["username"]}, '
+                      f'last login date: {user_info["last_login"]}')
+        print()
+
+    def handle(self, *args, **options):
+        print('Сервис начал свою работу... \n')
+        schedule.every().day.at('00:00').do(self.job)
+
+        while True:
+            schedule.run_pending()
+            time.sleep(1)
